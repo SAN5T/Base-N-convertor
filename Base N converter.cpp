1cb5081fdbcb3876bba10fb7172f4c58fe4a82f1
Sanjeev Tiwari <85246922+SAN5T@users.noreply.github.com>
2022-08-03 15:38:48 +0530

    Add files via upload

---
+++ Base N converter.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int convertToDec(char n1[], int b1){
+
+    int j=strlen(n1)-1, k=0;
+
+    long long int n=0;
+
+    while(j>=0){
+        int i=(int)n1[j--];
+        if(i>=48&&i<=57){
+            i-=48;      //0=48
+        }
+        else if(i>=65&&i<=90){
+            i-=55;      //10=A
+        }
+        else if(i>=97&&i<=122){
+            i-=61;      //36=a
+        }
+        else if(i==43||i==44){
+            i+=19;       //62=+
+        }
+        n= n + (i*pow(b1,k++));
+    }
+    return n;
+}
+
+void func(long long int n2, int b2){
+    if(n2>0){
+        func(n2/b2,b2);
+        int i= n2%b2;
+        if(i>=0&&i<=9){
+            i+=48;      //0=48
+        }
+        else if(i>=10&&i<=35){
+            i+=55;      //10=A
+        }
+        else if(i>=36&&i<=61){
+            i+=61;      //36=a
+        }
+        else if(i=='+'||i=='/'){
+            i-=19;       //62=+
+        }
+        cout << (char)i;
+    }
+}
+void convertToBase(char n1[], int b1, int b2){
+
+    long long int n2 = convertToDec(n1,b1);
+
+    if(n2==0)
+        cout << 0;
+
+    func(n2,b2);
+}
+
+int main(int i=1){
+
+    cout<<"*********~~~~~From Sanjeev Tiwari~~~~~~********** "<<endl;
+    cout<<"*******  This code will convert any base number to other base number *******"<<endl;
+
+
+
+    if(!i){
+        return 0;
+    }
+
+    char n1[100];
+    int b1, b2;
+
+    cout << "\n\n  Enter the Input string: ";
+    cin >> n1;
+    cout << "  Enter the base of input string: ";
+    cin >> b1;
+    cout << "  Enter the desired base: ";
+    cin >> b2;
+
+    cout << "  Output: ";
+    convertToBase(n1,b1,b2);
+
+    cout << "\n\n  Enter 0 to exit, 1 to continue ";
+    cin >> i;
+    cout << endl;
+    main(i);
+}
